43a2a65431554b9c352af15aa135d339507aea60
9exa <resosudarmo.wisesa@gmail.com>
2024-06-30 11:05:48 +1000

    FEAT:Added sounds to accomplishing habits

--- lib/gen/assets.gen.dart
+++ lib/gen/assets.gen.dart
@@ -7,11 +7,41 @@
 // ignore_for_file: type=lint
 // ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
 
+class $AssetsSoundsGen {
+  const $AssetsSoundsGen();
+
+  /// File path: assets/sounds/blob1.mp3
+  String get blob1 => 'assets/sounds/blob1.mp3';
+
+  /// File path: assets/sounds/blob2.mp3
+  String get blob2 => 'assets/sounds/blob2.mp3';
+
+  /// File path: assets/sounds/button-pressed-uppp.mp3
+  String get buttonPressedUppp => 'assets/sounds/button-pressed-uppp.mp3';
+
+  /// File path: assets/sounds/button_05.mp3
+  String get button05 => 'assets/sounds/button_05.mp3';
+
+  /// File path: assets/sounds/button_06.mp3
+  String get button06 => 'assets/sounds/button_06.mp3';
+
+  /// File path: assets/sounds/button_09.mp3
+  String get button09 => 'assets/sounds/button_09.mp3';
+
+  /// File path: assets/sounds/pop.mp3
+  String get pop => 'assets/sounds/pop.mp3';
+
+  /// List of all assets
+  List<String> get values =>
+      [blob1, blob2, buttonPressedUppp, button05, button06, button09, pop];
+}
+
 class Assets {
   Assets._();
 
   static const String buddySplash = 'assets/buddy_splash.svg';
   static const String homeSplash = 'assets/home_splash.svg';
+  static const $AssetsSoundsGen sounds = $AssetsSoundsGen();
 
   /// List of all assets
   static List<String> get values => [buddySplash, homeSplash];
